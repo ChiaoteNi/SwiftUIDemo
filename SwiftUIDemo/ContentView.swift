fb285c6038d8584eeaab7fd8ef73f25dd2ecf1e9
Chiao <aaron.chiao.19@gmail.com>
2019-06-18 02:29:34 +0800

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by admin on 2019/6/18.
+//  Copyright © 2019 aaronni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
